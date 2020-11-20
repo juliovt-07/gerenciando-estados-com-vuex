dd3070587a99e60b2c8e0c2703e6f2da0648d9bc
Júlio César <juliocesaar.ma@gmail.com>
2020-11-20 10:42:51 -0300

    Inicial Commit
    Gerenciamento básico com Vuex

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Counter: {{counter}}</h1>
+        <button @click="decrement">Decrement</button>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+    name: 'Counter',
+    computed: {
+        ...mapState ({
+            counter: state => state.counter
+        })
+    },
+    methods: {
+        ...mapMutations([
+            'decrement',
+            'increment'
+        ])
+    }
+}
+</script>
